1bed12ebf296d6c85f32bf2a54546ec04883785d
pavel-buran <pavel.buran@seznam.cz>
2026-06-10 23:10:57 +0200

    Commit-003

--- javascript/email.js
+++ javascript/email.js
@@ -1,4 +1,4 @@
-  // AJAX odesílání na pozadí
+  // AJAX odesílání na pozadí pro desktop
   var form = document.getElementById("comment_form");
   var statusDiv = document.getElementById("form_status");
 
@@ -23,6 +23,46 @@
         statusDiv.innerHTML = "Komentář byl úspěšně odeslán.";
         form.reset(); // Vymaže pole formuláře po úspěšném odeslání
         document.getElementById("submit_button").setAttribute("disabled", "disabled"); // Znovu uzamkne tlačítko
+      } else {
+        response.json().then(function(data) {
+          if (Object.hasOwn(data, 'errors')) {
+            statusDiv.innerHTML = data["errors"].map(error => error["message"]).join(", ");
+          } else {
+            statusDiv.innerHTML = "Nastala chyba při komunikaci.";
+          }
+        })
+      }
+    }).catch(function(error) {
+      statusDiv.style.color = "red";
+      statusDiv.innerHTML = "Formulář se nepodařilo odeslat. Zkontrolujte připojení.";
+    });
+  });
+
+  // AJAX odesílání na pozadí pro mobil
+  var form = document.getElementById("comment_form2");
+  var statusDiv = document.getElementById("form_status2");
+
+  form.addEventListener("submit", function(event) {
+    // Zamezí standardnímu přesměrování prohlížeče na Formspree
+    event.preventDefault(); 
+    
+    var data = new FormData(form);
+    statusDiv.style.color = "black";
+    statusDiv.innerHTML = "Odesílám komentář...";
+
+    // Odeslání dat asynchronně přes Fetch API
+    fetch("https://formspree.io/f/mnjyzyoa", {
+      method: "POST",
+      body: data,
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then(function(response) {
+      if (response.ok) {
+        statusDiv.style.color = "green";
+        statusDiv.innerHTML = "Komentář byl úspěšně odeslán.";
+        form.reset(); // Vymaže pole formuláře po úspěšném odeslání
+        document.getElementById("submit_button2").setAttribute("disabled", "disabled"); // Znovu uzamkne tlačítko
       } else {
         response.json().then(function(data) {
           if (Object.hasOwn(data, 'errors')) {
--- javascript/year.js
+++ javascript/year.js
@@ -15,4 +15,21 @@
   
   // Pojistka pro jistotu: Spustí se hned po načtení skriptu a tlačítko zamkne
   document.getElementById("submit_button").setAttribute("disabled", "disabled");
+
+  function validateYear2() {
+    var yearInput = document.getElementById("comment_year2");
+    var submitButton = document.getElementById("submit_button2");
+    
+    // Zjistí aktuální rok (2026)
+    var currentYear = new Date().getFullYear().toString();
+
+    // Kontrola hodnoty
+    if (yearInput.value.trim() === currentYear) {
+      submitButton.removeAttribute("disabled");
+    } else {
+      submitButton.setAttribute("disabled", "disabled");
+    }
+  }
   
+  // Pojistka pro jistotu: Spustí se hned po načtení skriptu a tlačítko zamkne
+  document.getElementById("submit_button2").setAttribute("disabled", "disabled");
